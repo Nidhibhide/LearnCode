ded6a8f4b06e9268992ebfca7c2c1295764902f2
Nidhi Bhide <158697336+Nidhibhide@users.noreply.github.com>
2025-06-06 09:24:26 +0530

    Update index.ts

--- backend/src/MongoDB/index.ts
+++ backend/src/MongoDB/index.ts
@@ -1,4 +1,4 @@
-// src/MongoDB/index.ts
+
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -8,9 +8,10 @@ dotenv.config();
 const DBConnect = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL as string);
-    console.log("MongoDB connected:", connection.connection.host);
+    console.log(" MongoDB connected:", connection.connection.host);
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error(" MongoDB connection error:", error);
+    process.exit(1); 
   }
 };
 
