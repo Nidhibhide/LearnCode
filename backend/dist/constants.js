30f4aea76824da69671df59d49ffae2af89b9d33
Nidhibhide <nidhibhide144@gmail.com>
2026-03-21 08:16:57 +0530

    update gitignore file

--- backend/dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Common constants for backend
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEST_LEVELS = exports.TEST_LANGUAGES = void 0;
-// Test programming languages
-exports.TEST_LANGUAGES = ["Java", "C++", "JavaScript", "Python", "C"];
-// Test difficulty levels
-exports.TEST_LEVELS = ["Basic", "Intermediate", "Advanced"];
-//# sourceMappingURL=constants.js.map
